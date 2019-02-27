c2f63b92b9fd19e3d3a23a2f41bf1f7cbcdedec9
Zhixing Chang <chzh5137@colorado.edu>
2019-02-27 16:51:50 -0700

    Setting up env
    Using Django and React
    create page folder containing example component

---
+++ cloudbase/frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Example from './Pages/Example'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Example/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ cloudbase/frontend/src/Pages/Example.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+
+class Example extends Component {
+    render() {
+        return (
+            <div className="example">
+                <center>
+                    <h1>Example</h1>
+                </center>
+            </div>
+        );
+    }
+}
+
+export default Example;
