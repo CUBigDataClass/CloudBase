b248204c828258f054b6113a482b9136d5f82043
Zhixing Chang <chzh5137@colorado.edu>
2019-03-06 16:16:12 -0700

    Code examples for react
    Add a test.js for example for making form page
    Add Test component to App.js

--- cloudbase/frontend/src/App.js
+++ cloudbase/frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Example from './Pages/Example'
+import Test from './Pages/test'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Example/>
+            <Example />
+            <Test/>
       </div>
     );
   }
